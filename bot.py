626f89670d71173bb31d26ab7e15b4c8f924f882
MCplayer00 <94725241+MeBadDev@users.noreply.github.com>
2022-11-21 10:11:16 +0800

    refactor stuff

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+import datas
+admins = datas.get_admin()
+bot_command = discord.SlashCommandGroup('bot','機器人')
+guild_id = [991254723745288303]
+
+@bot_command.command(description = '重新載入機器人',guild_ids = guild_id)
+async def reload_bot(ctx):
+	if ctx.author.id in admins:
+		datas.datas = datas.load_data()
+		await ctx.respond('正在重新載入!!')
+	else:
+		await ctx.respond('你沒有指令權限!!')
+
+
+@bot_command.command(description = '儲存機器人數據',guild_ids = guild_id)
+async def save_data(ctx):
+	file = discord.File('Datas.json')
+	await ctx.defer()
+	await ctx.followup.send(file=file,content = '好了 :D')
--- datas.py
+++ datas.py
@@ -1,30 +1,44 @@
 import json
 
 datas = {}
+
+
 def load_data():
-    with open('Coins.json','r') as coins:
-        datas['Coins'] = json.loads(coins.read())
-    with open('Admins.json','r') as admins:
-        datas['Admins'] = json.loads(admins.read())
-    
+	with open('Datas.json','r') as dats:
+		return json.loads(dats.read())
+
 def save_data():
-    f = open('Coins.json','w')
-    
-    f.write(json.dumps(datas['Coins'],indent=4))
-    f.close()
+	f = open('Datas.json','w')
+	f.write(json.dumps(datas,indent=4))
+	f.close()
+
+
 def add_coin(user,amount):
-    load_data()
-    if str(user) in datas['Coins']:
-        datas['Coins'][str(user)] += amount
-    else:
-        datas['Coins'][str(user)] = 0
-    save_data()
+	if str(user) in datas['Coins']:
+		datas['Coins'][str(user)] += amount
+	else:
+		datas['Coins'][str(user)] = 0
+	save_data()
+
+
+def set_coin(user,amount):
+	if str(user) in datas['Coins']:
+		datas['Coins'][str(user)] = amount
+	else:
+		datas['Coins'][str(user)] = 0
+	save_data()
+
+
 def get_coin(user):
-    if str(user) in datas['Coins']:
-        return datas['Coins'][str(user)]
-    else:
-        datas['Coins'][str(user)] = 0
-        save_data()
-        return 0 
-    
-load_data()
+	if str(user) in datas['Coins']:
+		save_data()
+		return datas['Coins'][str(user)]
+	else:
+		datas['Coins'][str(user)] = 0
+		save_data()
+		return 0
+
+def get_admin():
+	return datas['Admins']
+
+datas = load_data()
